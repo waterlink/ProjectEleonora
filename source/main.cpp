f934f5a0c97433e251c083538d23b9f21916ccd5
Alexey Fedorov <waterlink000@gmail.com>
2011-06-01 20:47:01 +0300

    done with command line input

--- source/main.cpp
+++ source/main.cpp
@@ -17,9 +17,28 @@ arising from the use of this software.
 
 #include "main.hpp"
 
+#include <string>
+#include <cstdio>
+
+using namespace std;
+
 int main(int argc, char ** argv){
 
+	long long x;
+
+	if (argc < 2){
 	
+		printf("usage: x Part Id\n");
+		return 0;
+	
+	}
+	
+	string Part = argv[1];
+	
+	string IdString = argv[2];
+	sscanf(IdString.c_str(), "%lld", &x);
+	
+	long long Id = x;
 
 	return 0;
 
