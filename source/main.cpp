ea00c488b4e01a7092ced2c14da2f3f7a5461864
Alexey Fedorov <waterlink000@gmail.com>
2011-06-01 20:38:20 +0300

    added main source file, it is only main function

---
+++ source/main.cpp
@@ -0,0 +1,27 @@
+/*Copyright (c) 2011 Fedorov Alexey.
+
+Author: Fedorov Alexey.
+Type: Open-Source Project.
+Platform: Linux.
+Codename: Project Eleonora.
+
+Student tickets accounting system.
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+*/
+
+/*source main
+*/
+
+#include "main.hpp"
+
+int main(int argc, char ** argv){
+
+	
+
+	return 0;
+
+}
+
---
+++ source/main.hpp
@@ -0,0 +1,25 @@
+/*Copyright (c) 2011 Fedorov Alexey.
+
+Author: Fedorov Alexey.
+Type: Open-Source Project.
+Platform: Linux.
+Codename: Project Eleonora.
+
+Student tickets accounting system.
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+*/
+
+/*header main
+*/
+
+#ifndef __main_H__
+#define __main_H__
+
+
+
+#endif /* __main_H__ */
+
+
